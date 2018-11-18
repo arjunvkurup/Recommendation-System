4d0b567ca0ceb1f05eb1fd0e00c3c41488751c56
imarjunv <arjunvijayanathakurup@gmail.com>
2018-11-18 12:41:58 +0530

    update

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from geopy.geocoders import GoogleV3
 from ..utils.client1 import *
 from ..utils.client2 import *
 from ..utils.client3 import *
+from ..utils.parallel import *
 from multiprocessing import Process
 
 
@@ -12,77 +13,79 @@ from multiprocessing import Process
 def index(request):
     template = loader.get_template('blog/index.html')
 
+    data_1
+    data_2
+    data_3
 
-    ''' Test Area  '''
+    def fun_call1():
+        print('Client 1 call')
+        global data_1
+        data_1 = Server1()
+        print(data_1)
 
-    # LongLatCount1[][]
-    # LongLatCount2[][]
-    # LongLatCount3[][]
-    #
-    # def fun_call1():
-    #     print('Client 1 call')
-    #     # global data_1
-    #     LongLatCount1 = Server1()
-    #
-    # def fun_call2():
-    #     print('Client 2 call')
-    #     # global data_2
-    #     LongLatCount2 = Server2()
-    #
-    # def fun_call3():
-    #     print('Client 3 call')
-    #     # global data_3
-    #     LongLatCount3 = Server3()
-    #
-    # p1 = Process(target=fun_call1())
-    # p2 = Process(target=fun_call2())
-    # p3 = Process(target=fun_call3())
-    # p1.start()
-    # p2.start()
-    # p3.start()
-    # p1.join()
-    # p2.join()
-    # p3.join()
+    def fun_call2():
+        print('Client 2 call')
+        global data_2
+        data_2 = Server2()
+        print(data_2)
+
+    def fun_call3():
+        print('Client 3 call')
+        global data_3
+        data_3 = Server3()
+        print(data_3)
+
+    p1 = Process(target=fun_call1())
+    p2 = Process(target=fun_call2())
+    p3 = Process(target=fun_call3())
+    p1.start()
+    p2.start()
+    p3.start()
+    p1.join()
+    p2.join()
+    p3.join()
 
 
-    ''' Test Area Ends '''
 
-    # LongLatCount1 = [
-    #     [[-159.38, 22.08], 74],
-    #     [[-157.83, 21.31], 807],
-    #     [[-156.54, 20.9], 0],
-    #     [[-158.01, 21.54], 0],
-    #     [[-158.0, 21.38], 495],
-    #     [[-159.56, 21.93], 10],
-    #     [[-156.05, 20.75], 0],
-    #     [[-156.36, 20.81], 28],
-    #     [[-158.17, 21.43], 98],
-    #     [[-157.75, 21.37], 22]
-    # ]
-    # LongLatCount2 = [
-    #     [[-159.38, 22.08], 74],
-    #     [[-157.83, 21.31], 807],
-    #     [[-156.54, 20.9], 0],
-    #     [[-158.01, 21.54], 0],
-    #     [[-158.0, 21.38], 995],
-    #     [[-159.56, 21.93], 10],
-    #     [[-156.05, 20.75], 0],
-    #     [[-156.36, 20.81], 28],
-    #     [[-158.17, 21.43], 98],
-    #     [[-157.75, 21.37], 22]
-    # ]
-    # LongLatCount3 = [
-    #     [[-159.38, 22.08], 74],
-    #     [[-157.83, 21.31], 7],
-    #     [[-156.54, 20.9], 0],
-    #     [[-158.01, 21.54], 0],
-    #     [[-158.0, 21.38], 5],
-    #     [[-159.56, 21.93], 10],
-    #     [[-156.05, 20.75], 0],
-    #     [[-156.36, 20.81], 28],
-    #     [[-158.17, 21.43], 98],
-    #     [[-157.75, 21.37], 22]
-    # ]
+    # LongLatCount1 =
+    # LongLatCount2 = parallel.data_2
+    # LongLatCount3 = parallel.data_3
+    LongLatCount1 = [
+        [[-159.38, 22.08], 74],
+        [[-157.83, 21.31], 807],
+        [[-156.54, 20.9], 0],
+        [[-158.01, 21.54], 0],
+        [[-158.0, 21.38], 495],
+        [[-159.56, 21.93], 10],
+        [[-156.05, 20.75], 0],
+        [[-156.36, 20.81], 28],
+        [[-158.17, 21.43], 98],
+        [[-157.75, 21.37], 22]
+    ]
+    LongLatCount2 = [
+        [[-159.38, 22.08], 74],
+        [[-157.83, 21.31], 807],
+        [[-156.54, 20.9], 0],
+        [[-158.01, 21.54], 0],
+        [[-158.0, 21.38], 995],
+        [[-159.56, 21.93], 10],
+        [[-156.05, 20.75], 0],
+        [[-156.36, 20.81], 28],
+        [[-158.17, 21.43], 98],
+        [[-157.75, 21.37], 22]
+    ]
+    LongLatCount3 = [
+        [[-159.38, 22.08], 74],
+        [[-157.83, 21.31], 7],
+        [[-156.54, 20.9], 0],
+        [[-158.01, 21.54], 0],
+        [[-158.0, 21.38], 5],
+        [[-159.56, 21.93], 10],
+        [[-156.05, 20.75], 0],
+        [[-156.36, 20.81], 28],
+        [[-158.17, 21.43], 98],
+        [[-157.75, 21.37], 22]
+    ]
     max1 = LongLatCount1[0][1]
     max_index1 = 0
     for i in range(len(LongLatCount1)):
